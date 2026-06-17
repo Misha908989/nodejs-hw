95dc00dbde2cd663cda006b8881dd96e42ceeb9a
Misha908989 <mishazaharevich71@gmail.com>
2026-06-17 23:47:57 +0300

    last try

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -20,7 +20,9 @@ export const createNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate(noteId, req.body, { new: true });
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
+    returnDocument: 'after',
+  });
   if (!note) throw createHttpError(404, 'Note not found');
   res.status(200).json(note);
 };
