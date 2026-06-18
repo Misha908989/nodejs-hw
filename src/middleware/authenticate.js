95553d773c080e1939cd3bcca5bb8714d3d56426
Misha908989 <mishazaharevich71@gmail.com>
2026-06-18 15:10:20 +0300

    try

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,26 +3,30 @@ import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
-  const { accessToken } = req.cookies;
+  try {
+    const { accessToken } = req.cookies;
 
-  if (!accessToken) {
-    throw createHttpError(401, 'Missing access token');
+    if (!accessToken) {
+      throw createHttpError(401, 'Missing access token');
+    }
+
+    const session = await Session.findOne({ accessToken });
+    if (!session) {
+      throw createHttpError(401, 'Session not found');
+    }
+
+    if (new Date() > session.accessTokenValidUntil) {
+      throw createHttpError(401, 'Access token expired');
+    }
+
+    const user = await User.findById(session.userId);
+    if (!user) {
+      throw createHttpError(401);
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const session = await Session.findOne({ accessToken });
-  if (!session) {
-    throw createHttpError(401, 'Session not found');
-  }
-
-  if (new Date() > session.accessTokenValidUntil) {
-    throw createHttpError(401, 'Access token expired');
-  }
-
-  const user = await User.findById(session.userId);
-  if (!user) {
-    throw createHttpError(401);
-  }
-
-  req.user = user;
-  next();
 };
