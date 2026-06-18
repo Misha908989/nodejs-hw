79425802e6f7dc46c3e393dcff2150efe0ff269d
Misha908989 <mishazaharevich71@gmail.com>
2026-06-18 20:56:47 +0300

    last try

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -10,6 +10,10 @@ export const authenticate = async (req, res, next) => {
       throw createHttpError(401, 'Missing access token');
     }
 
+    if (!sessionId) {
+      throw createHttpError(401, 'Session not found');
+    }
+
     const session = await Session.findOne({ _id: sessionId, accessToken });
     if (!session) {
       throw createHttpError(401, 'Session not found');
