e69f7ff026be4a6b9e9c23b4d522123fbae9286c
Misha908989 <mishazaharevich71@gmail.com>
2026-06-18 08:03:37 +0300

    try

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
+import { authenticate } from './middleware/authenticate.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,8 +18,10 @@ const app = express();
 app.use(logger);
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
-app.use(notesRoutes);
+app.use(authRoutes);
+app.use(authenticate, notesRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
